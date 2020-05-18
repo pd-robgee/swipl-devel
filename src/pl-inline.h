7f9433c8cf1a4becd0fd69c21e09ade904c5248d
Röbke Geenen <rbke.geenen@prodrive-technologies.com>
2020-05-18 23:42:00 +0200

    Testing

--- src/pl-inline.h
+++ src/pl-inline.h
@@ -42,6 +42,10 @@
 #include <windows.h>
 #undef small
 #include <intrin.h>
+#if SIZEOF_VOIDP == 8
+  #pragma intrinsic(_BitScanReverse64)
+#endif
+#pragma intrinsic(_BitScanReverse)
 #endif
 
 		 /*******************************
@@ -68,11 +72,9 @@ MSB(size_t i)
 { unsigned long index;
 #if SIZEOF_VOIDP == 8
   unsigned __int64 mask = i;
-  #pragma intrinsic(_BitScanReverse64)
   _BitScanReverse64(&index, mask);
 #else
   unsigned long mask = i;
-  #pragma intrinsic(_BitScanReverse)
   _BitScanReverse(&index, mask);
 #endif
 
@@ -84,7 +86,6 @@ MSB(size_t i)
 static inline int
 MSB64(int64_t i)
 { unsigned long index;
-  #pragma intrinsic(_BitScanReverse64)
   _BitScanReverse64(&index, i);
   return index;
 }
