0f9935ab54a87b3e1fb572fc7217486e239a360e
Röbke Geenen <rbke.geenen@prodrive-technologies.com>
2020-05-18 20:23:20 +0200

    Testing

--- src/pl-inline.h
+++ src/pl-inline.h
@@ -67,10 +67,10 @@ MSB(size_t i)
 { unsigned long index;
 #if SIZEOF_VOIDP == 8
   unsigned __int64 mask = i;
-  BitScanReverse64(&index, mask);
+  _BitScanReverse64(&index, mask);
 #else
   unsigned long mask = i;
-  BitScanReverse(&index, mask);
+  _BitScanReverse(&index, mask);
 #endif
 
   return index;
@@ -80,7 +80,7 @@ MSB(size_t i)
 static inline int
 MSB64(int64_t i)
 { unsigned long index;
-  BitScanReverse64(&index, i);
+  _BitScanReverse64(&index, i);
   return index;
 }
 
